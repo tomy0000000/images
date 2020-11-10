043cddda3196798e7e5a8b8a54a4e54aa30590a5
Tomy Hsieh <tomy0000000@gmail.com>
2020-11-11 03:09:05 +0800

    First Draft for v2

--- Dockerfiles/integrated-dev/latest/config/init.sh
+++ Dockerfiles/integrated-dev/latest/config/init.sh
@@ -3,23 +3,13 @@
 # Create User
 username="${AWESOME_USER:-user}"
 password="${AWESOME_PASSWORD:-password}"
-useradd $username --groups "root" --home-dir "/home/$username" --shell $(which zsh)
+useradd $username --groups "root,sudo" --home-dir "/home/$username"
 echo "$username:$password" | chpasswd
 cp -r /etc/skel/. "/home/$username"
 chown "$username:$username" -R "/home/$username"
 
 # Start SSH server
 service ssh start > /dev/null
-
-# Setting Password
-sudo -i -u $username python /home/$username/set_password.py $username $password
-rm "home/$username/set_password.py"
-
-# JupyterLab
-sudo -bi -u $username jupyter lab 2>&1
-
-# Code Server
-sudo -bi -u $username code-server 2>&1
 
 # Spawn Shell
 # su $username --login
