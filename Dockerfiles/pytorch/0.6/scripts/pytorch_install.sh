6507dfa2382272168abb74b725634dd1ae8c41ec
Tomy Hsieh <tomy0000000@gmail.com>
2020-12-04 13:49:22 +0800

    Patch PyTorch 0.6

---
+++ Dockerfiles/pytorch/0.6/scripts/pytorch_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ ${CUDA_VERSION} = 10.2* ]];
+then
+    pip install torch==${PYTORCH_VERSION}
+else
+    CUDA_VERSION=`echo ${CUDA_VERSION} | cut -d'.' -f'1-2' | sed 's/\.//g'`
+    pip install torch==${PYTORCH_VERSION}+cu${CUDA_VERSION} --find-links https://download.pytorch.org/whl/torch_stable.html
+fi
