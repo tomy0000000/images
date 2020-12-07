80093fbdbafa134767770c11967bc28a34c33bfc
Tomy Hsieh <tomy0000000@gmail.com>
2020-12-07 17:13:49 +0800

    Organize Scripts

--- Dockerfiles/pytorch/0.6/scripts/python_install.sh
+++ Dockerfiles/pytorch/0.6/scripts/python_install.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
+# Add deadsnake PPA
 add-apt-repository ppa:deadsnakes/ppa
 apt update
 apt install -y python${PYTHON_VERSION}
 
-if [ ${UBUNTU_VERSION} == "18.04" ] && [ ${PYTHON_VERSION} == "3.6" ]
+# Manually install distutils for Python 3.8 or Ubuntu-provided Python
+# 18.04 (bionic): Python 3.6
+# 20.04 (focal): Python 3.8
+if [[ ${UBUNTU_VERSION} == "18.04" ] && [ ${PYTHON_VERSION} == "3.6" ]] || [ ${PYTHON_VERSION} == "3.8" ]
 then
     apt install -y python${PYTHON_VERSION}-distutils
-    
-    update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 1
-    update-alternatives --config python
-    update-alternatives --set python /usr/bin/python${PYTHON_VERSION}
-elif [ ${UBUNTU_VERSION} == "20.04" ] && [ ${PYTHON_VERSION} == "3.8" ]
-then
-    apt install -y python${PYTHON_VERSION}-distutils
-    
-    update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 1
-    update-alternatives --config python
-    update-alternatives --set python /usr/bin/python${PYTHON_VERSION}
-else
-    if [ ${UBUNTU_VERSION} == "16.04" ] && [ ${PYTHON_VERSION} == "3.8" ]
-    then
-        apt install -y python${PYTHON_VERSION}-distutils
-    elif [ ${UBUNTU_VERSION} == "18.04" ] && [ ${PYTHON_VERSION} == "3.8" ]
-    then
-        apt install -y python${PYTHON_VERSION}-distutils
-    fi
-    
-    update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 1
-    update-alternatives --config python
-    update-alternatives --set python /usr/bin/python${PYTHON_VERSION}
 fi
+
+update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 1
+update-alternatives --config python
+update-alternatives --set python /usr/bin/python${PYTHON_VERSION}
 
 curl https://bootstrap.pypa.io/get-pip.py | sudo -H python
