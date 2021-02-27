07c89ac6dc3f1e6f0fb262d6b4a42eb3ff2af690
Tomy Hsieh <tomy0000000@gmail.com>
2021-02-28 03:33:14 +0800

    Pytorch: cancel py3.9 support

--- Dockerfiles/pytorch/scripts/pytorch_install.sh
+++ Dockerfiles/pytorch/scripts/pytorch_install.sh
@@ -5,7 +5,7 @@ if [[ ${CUDA_VERSION} = 10.2* ]]
 then
     pip install "torch==${PYTORCH_VERSION}"
 else
-    CUDA_VERSION=$(echo "${CUDA_VERSION}" | cut -d'-' -f'1' | sed 's/\.//g')
+    CUDA_VERSION=$(echo "${CUDA_VERSION}" | cut -d'.' -f'1-2' | cut -d'-' -f'1' | sed 's/\.//g')
     pip install "torch==${PYTORCH_VERSION}+cu${CUDA_VERSION}" --find-links https://download.pytorch.org/whl/torch_stable.html
 fi
 
