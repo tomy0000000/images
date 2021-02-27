502719486f9e0334c4628e74f137fe2c8e007fa6
Tomy Hsieh <tomy0000000@gmail.com>
2021-02-28 01:28:57 +0800

    Pytorch: Add distutils install on certain version

--- Dockerfiles/pytorch/scripts/python_install.sh
+++ Dockerfiles/pytorch/scripts/python_install.sh
@@ -8,8 +8,8 @@ apt install -y "python${PYTHON_VERSION}" "python${PYTHON_VERSION}-dev"
 
 # Manually install distutils for Python 3.8 or Ubuntu-provided Python
 # 18.04 (bionic): Python 3.6
-# 20.04 (focal): Python 3.8
-if [[ ( ${UBUNTU_VERSION} == "18.04" && ${PYTHON_VERSION} == "3.6" ) ]] || [[ ${PYTHON_VERSION} == "3.8" ]]
+# Python 3.8 & Python 3.9
+if [[ ( ${UBUNTU_VERSION} == "18.04" && ${PYTHON_VERSION} == "3.6" ) ]] || [[ ${PYTHON_VERSION} == "3.8" ]] || [[ ${PYTHON_VERSION} == "3.9" ]]
 then
     apt install -y "python${PYTHON_VERSION}-distutils"
 fi
